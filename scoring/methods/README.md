deda1e3c52e8ac6fc035e12edcd5703d3120a043
spring1253 <1253spring@snu.ac.kr>
2023-12-16 02:50:28 +0900

    updated scoring methods

---
+++ scoring/methods/README.md
@@ -0,0 +1,20 @@
+# Scoring Methods
+
+| Task | Scoring Method |
+| -------- | -------- |
+| ko_quiz_1 | multiple_choice |
+| ko_quiz_2 | multiple_choice |
+| ko_quiz_3 | ko_quiz_3 |
+| ko_quiz_4 | multiple_choice |
+| ko_quiz_5 | multiple_choice |
+| ko_quiz_6 | multiple_choice |
+| ko_quiz_7 | multiple_choice |
+| ko_quiz_8 | multiple_choice |
+| nli | multiple_choice |
+| number_1 | number_1 |
+| number_2 | multiple_choice |
+| number_3 | number_3 |
+| reasoning | reasoning |
+| spelling_correct | ??? |
+| summarization | summarization |
+| translation | translation |
---
+++ scoring/methods/ko_quiz_3/eval_ko_quiz_3.py
@@ -0,0 +1,60 @@
+import re, os, csv
+
+# Settings
+reference_path = "ex_reference.csv"
+generated_path = "ex_generated.csv"
+result_path = "ex_result.csv"
+
+reference_file = open(reference_path, "rt", encoding='utf8', newline='')
+generated_file = open(generated_path, "rt", encoding='utf8', newline='')
+result_file = open(result_path, "wt", encoding='utf8', newline='')
+
+ref_reader = csv.reader(reference_file)
+gen_reader = csv.reader(generated_file)
+writer = csv.writer(result_file)
+
+next(ref_reader)
+next(gen_reader)
+
+ref_data = [row for row in ref_reader]
+gen_data = [row for row in gen_reader]
+
+task_name, model_name = gen_data[0][0], gen_data[0][3]
+
+for ref_row, gen_row in zip(ref_data, gen_data):
+    if ref_row[1] != gen_row[1]: # index not match
+        print(f"Ref: {ref_row[0]} {ref_row[1]} / Gen: {gen_row[0]} {gen_row[1]}")
+        print("Error: index not match!")
+        exit()
+    
+    if ref_row[0] != task_name: # task mixed in reference
+        print(ref_row)
+        print("Error: task mixed in reference!")
+        exit()
+    
+    if gen_row[0] != task_name: # task mixed in generated
+        print(gen_row)
+        print("Error: task mixed in generated!")
+        exit()
+    
+    if gen_row[3] != model_name: # model mixed in generated
+        print(gen_row)
+        print("Error: model mixed in generated!")
+        exit()
+    
+print("Matching successful!")
+
+def extract_numbers(input_string):
+    # Use regular expression to find all numbers in the string
+    numbers = re.findall(r'\d+', input_string)
+    return list(set(numbers))
+
+def score(gen, ref):
+    if ref.isdigit(): # multiple choice
+        return extract_numbers(gen) == [ref]
+    return ref in gen
+
+writer.writerow(['task_name', 'index', 'model_name', 'prompt', 'output', 'score'])
+
+for ref_row, gen_row in zip(ref_data, gen_data):
+    writer.writerow([task_name, ref_row[1], model_name, ref_row[2], gen_row[2], 1 if score(gen_row[2], ref_row[3]) else 0])
