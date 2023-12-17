58fa788c8cadd782fcf20853e48130ec63eb3143
spring1253 <1253spring@snu.ac.kr>
2023-12-17 22:54:38 +0900

    changed result format

--- scoring/result/calculate_result.py
+++ scoring/result/calculate_result.py
@@ -9,30 +9,34 @@ def calculate_average(file_path):
         return sum(values) / len(values)
 
 def process_directory(directory):
-    result_file_path = os.path.join(directory, f'{os.path.basename(directory)}_result.csv')
+    data = []
+
+    for filename in os.listdir(directory):
+        if filename.endswith('.csv') and filename != f'{os.path.basename(directory)}_result.csv':
+            file_path = os.path.join(directory, filename)
+            average = calculate_average(file_path)
+            data.append([filename, average])
     
-    with open(result_file_path, 'w', newline='') as result_file:
-        writer = csv.writer(result_file)
-        writer.writerow(['task', 'average score'])
+    data.sort(key=lambda x: x[0])
 
-        data = []
-
-        for filename in os.listdir(directory):
-            if filename.endswith('.csv') and filename != f'{os.path.basename(directory)}_result.csv':
-                file_path = os.path.join(directory, filename)
-                average = calculate_average(file_path)
-                data.append([filename, average])
-        
-        data.sort(key=lambda x: x[0])
-
-        writer.writerows(data)
-
-    print(f'Processed: {result_file_path}')
+    return (directory, data)
 
 def main():
+    tasks = ['ko_quiz_1', 'ko_quiz_2', 'ko_quiz_3', 'ko_quiz_4', 'ko_quiz_5', 'ko_quiz_6', 'ko_quiz_7', 'ko_quiz_8',
+             'nli', 'number_1', 'number_2', 'number_3', 'reasoning', 'spelling_correct', 'summarization', 'translation']
+    data = []
     for root, dirs, files in os.walk('.'):
         for directory in dirs:
-            process_directory(os.path.join(root, directory))
+            result = process_directory(os.path.join(root, directory))
+            if [i[0].replace('.csv', '') for i in result[1]] != tasks:
+                print('Error: missing data in', directory)
+                continue
+            data.append((result[0], [i[1] for i in result[1]]))
+    
+    writer = csv.writer(open('result.csv', mode='w', encoding='utf8', newline=''))
+    writer.writerow(['task_name'] + [i[0].replace('./', '') for i in data])
+    for i in range(len(tasks)):
+        writer.writerow([tasks[i]] + [row[1][i] for row in data])
 
 if __name__ == "__main__":
     main()
