32c90a39ab2e193671400a9c13b276301507e3e6
spring1253 <1253spring@snu.ac.kr>
2023-12-17 16:37:34 +0900

    llama2_13b evaluate 1st trial

--- scoring/divide_by_task.py
+++ scoring/divide_by_task.py
@@ -1,18 +1,31 @@
-import pandas as pd
+import os, pandas as pd
 
-# Load the CSV file into a DataFrame
-csv_file_path = 'result.csv'
-df = pd.read_csv(csv_file_path)
+# List of CSV file paths
+csv_file_paths = [# '/TaskExecutor/kullm5.8b/result.csv',
+                  # '/TaskExecutor/kullm12.8b/result.csv',
+                  '/TaskExecutor/llama2_13b/result.csv']
 
-# Extract the part of 'task_name' before '/'
-df['group_name'] = df['task_name'].apply(lambda x: 'translation' if '/' in x else x)
+current_position = os.getcwd()
+current_position = current_position[current_position.find('/TaskExecutor'):]
 
-# Group the DataFrame by the new 'group_name' column
-grouped = df.groupby('group_name')
+for csv_file_path in csv_file_paths:
+    # Load the CSV file into a DataFrame
+    relative_path = os.path.relpath(csv_file_path, current_position)
+    df = pd.read_csv(relative_path)
 
-# Create separate CSV files for each group
-for group_name, group_df in grouped:
-    output_file_path = f'temp/{group_name}_output.csv'
-    group_df.drop(columns=['group_name'], inplace=True)  # Remove the temporary column
-    group_df.to_csv(output_file_path, index=False)
-    print(f'File created: {output_file_path}')
+    # Task is 'translation' if {task_name} includes '/' character
+    df['group_name'] = df['task_name'].apply(lambda x: 'translation' if '/' in x else x)
+
+    # Group the DataFrame by the new 'group_name' column
+    grouped = df.groupby('group_name')
+
+    # Create separate CSV files for each group
+    for group_name, group_df in grouped:
+        output_file_path = relative_path[:relative_path.rfind('/')] + '/grouped_results/' + group_name + '_result.csv'
+
+        if not os.path.exists(relative_path[:relative_path.rfind('/')] + '/grouped_results'):
+            os.makedirs(relative_path[:relative_path.rfind('/')] + '/grouped_results')
+
+        group_df.drop(columns=['group_name'], inplace=True)  # Remove the temporary column
+        group_df.to_csv(output_file_path, index=False)
+        print(f'File created: {output_file_path}')
