8aca3b783962b53f102f080f48921c90139bb73b
red1108 <minkyu.lee65@gmail.com>
2023-12-20 02:58:27 +0900

    add koalpaca

--- ko_gpt_trinity/inference.py
+++ ko_gpt_trinity/inference.py
@@ -3,6 +3,11 @@ import requests
 API_URL = "https://api-inference.huggingface.co/models/skt/ko-gpt-trinity-1.2B-v0.5"
 headers = {"Authorization": "Bearer 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"}
 
+def messages_to_string(prompt: str):
+    return f"### 질문: {prompt}\n\n### 답변:"
+
+
+
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
@@ -12,7 +17,7 @@ def query(payload):
 # output: 문장을 이어서 완성시킨 문장 ex) 1919년 일어난 3.1운동은 독립신문을 통해 전국적으로 알려지게 되었다. 3.1'
 output = query(
     {
-	"inputs": "830분의 465을 수로 바꾸어서 출력해줘",
+	"inputs": messages_to_string("AI에 대해 설명해줘"),
     }
 )
 
